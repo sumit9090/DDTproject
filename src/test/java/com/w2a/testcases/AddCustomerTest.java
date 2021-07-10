ca855c113046958fb0d59389a1df242566acffb1
sumit9090 <sumit.kalra121@gmail.com>
2021-07-10 11:26:32 +0530

    firstcommit

---
+++ src/test/java/com/w2a/testcases/AddCustomerTest.java
@@ -0,0 +1,42 @@
+package com.w2a.testcases;
+
+import java.util.Hashtable;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.w2a.base.TestBase;
+import com.w2a.utilities.TestUtil;
+
+
+public class AddCustomerTest extends TestBase {
+
+	@Test(dataProviderClass=TestUtil.class,dataProvider="dp")
+	public void addCustomerTest(Hashtable<String,String> data) throws InterruptedException
+	{
+		click("addCustBtn_CSS");
+		
+		Thread.sleep(5000);
+		
+		//driver.findElement(By.cssSelector(OR.getProperty("firstname_CSS"))).sendKeys("1");
+		type("firstname_CSS",data.get("firstname"));
+		Thread.sleep(2000);
+		//driver.findElement(By.xpath(OR.getProperty("lastname_XPATH"))).sendKeys("1");
+		type("lastname_XPATH",data.get("lastname"));
+		Thread.sleep(2000);
+		//driver.findElement(By.cssSelector(OR.getProperty("postcode_CSS"))).sendKeys("1");
+		type("postcode_CSS",data.get("postcode"));
+		Thread.sleep(2000);
+		
+		click("addbtn_CSS");
+		
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		alert.accept();
+		Thread.sleep(3000);
+		Reporter.log("adding customer message ");
+		
+	}
+
+}
